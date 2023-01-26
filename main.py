64e45fdba6e754a6a5d188f219221ddf3df33018
김세현 <dnjfdid14@naver.com>
2023-01-26 10:44:12 +0900

    ⚡️ :: element 찾기

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 option = webdriver.ChromeOptions()
 option.add_argument("window-size=1280,720")
 option.add_argument("no-sandbox")
-# option.add_argument("headless")
 
-browser = webdriver.Chrome(ChromeDriverManager().install(), options=option)
-browser.get("https://shopping.naver.com/")
-time.sleep(3)
-browser.close()
+chrome = webdriver.Chrome(ChromeDriverManager().install(), options=option)
+chrome.get("https://shopping.naver.com/")
+wait = WebDriverWait(chrome, 10)
+el = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[type=text]")))
+print(el)
+chrome.close() 
